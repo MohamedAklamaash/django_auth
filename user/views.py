8256fe1c87f3072ed56794ba9e2ae66b6b8c4130
MohamedAklamaash <aklamaash78@gmail.com>
2025-03-08 15:16:55 +0530

    user view 1 done

--- user/views.py
+++ user/views.py
@@ -2,6 +2,20 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import User
+from .models import UserModel
 from .serializers import UserSerializer
 
+class UserView(APIView):
+    
+    def get(self, request):
+        usrs = UserModel.objects.all()
+        serialized_users = UserSerializer(usrs, many=True)
+        return Response(serialized_users)
+
+    def post(self, request):
+        serializer = UserSerializer(data = request.data)
+        if serializer.is_valid(): # username, email, password 
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
