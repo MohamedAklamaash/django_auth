d15c8ba3b436d092fe3a8fceae460610f4993c9c
MohamedAklamaash <aklamaash78@gmail.com>
2025-03-09 16:30:15 +0530

    signin is working

--- custom_auth/urls.py
+++ custom_auth/urls.py
@@ -1,6 +1,7 @@
 from rest_framework.urls import path
-from .views import SignupUser
+from .views import SignupUser,SigninView
 
 urlpatterns = [
-    path("signup",SignupUser.as_view(),name="signup-user")
+    path("signup",SignupUser.as_view(),name="signup-user"),
+    path("signin",SigninView.as_view(),name="signin-view")
 ]
